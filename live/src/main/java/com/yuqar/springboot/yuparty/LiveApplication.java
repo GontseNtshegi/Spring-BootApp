aa81914d9d964f5b358640995ed564fd3fce19cc
Gontse <gontse.ntshegi@gmail.com>
2016-11-16 09:56:12 +0200

    Update LiveApplication.java

--- live/src/main/java/com/yuqar/springboot/yuparty/LiveApplication.java
+++ live/src/main/java/com/yuqar/springboot/yuparty/LiveApplication.java
@@ -3,6 +3,8 @@ package com.yuqar.springboot.yuparty;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+
 @SpringBootApplication
 public class LiveApplication {
 
